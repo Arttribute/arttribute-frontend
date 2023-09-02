a61f5fa6d3a4b1b810683881c3a3e3d92d6d0255
@bashybaranaba <bashybaranaba@gmail.com>
2023-09-02 16:12:16 +0300

    create user

--- app/page.tsx
+++ app/page.tsx
@@ -25,9 +25,32 @@ export default function Home() {
     const data = await response.json();
     console.log(data);
   }
+
+  async function createUser() {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.BrowserProvider(connection);
+    const signer = await provider.getSigner();
+    const address = await signer.getAddress();
+    const message = "Create user";
+    const userName = "Baranaba";
+    const signature = await signer.signMessage(message);
+    //post to server
+    const response = await fetch("http://localhost:5000/users/createuser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ address, message, signature, name: userName }),
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+
   return (
     <main className={styles.main}>
       <button onClick={signMessage}>Sign Message</button>
+      <button onClick={createUser}>Create User</button>
     </main>
   );
 }
